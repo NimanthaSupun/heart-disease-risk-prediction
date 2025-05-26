b96346fd4c74fab66ad9f4a12089c7fd5a13bab5
Nimantha Supun <supunnimantha199@gmail.com>
2025-05-26 11:43:56 +0530

    feat: implement prediction endpoint with input data validation

--- server/app.py
+++ server/app.py
@@ -18,3 +18,21 @@ app.add_middleware(
 
 # load model
 model = tf.keras.models.load_model("model/model-192.keras")
+
+# define input structure
+
+class InputData(BaseModel):
+    feature1:float
+    feature2:float
+    feature3:float
+    feature4:float
+    feature5:float
+    feature6:float
+    feature7:float
+
+@app.post("/predict")
+def predict(data: InputData):
+    input_array = np.array([[data.feature1,data.feature2,data.feature3,data.feature4,data.feature4,data.feature5,data.feature6,data.feature7]])
+    prediction = model.predict(input_array)
+    risk_percent = np.clip(prediction[0][0],0.1) * 100
+    return {"risk_precent"> round(risk_percent,2)}
