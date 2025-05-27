fd16e5792cab8b99b2c12182ff77319bf59f916d
Nimantha Supun <supunnimantha199@gmail.com>
2025-05-27 10:14:47 +0530

    refactor: remove unused components from App and delete Predict component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-import Predict from './components/predict/Predict'
-import Test from './components/test'
-import HeartDiseasePredictor from './components/predict/HeartDiseasePredictor'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
    <div>
-   <HeartDiseasePredictor/>
    </div>
   )
 }
